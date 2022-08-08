a9deff2fe3106c250c8ed7bb7b97a5ea5ad05191
Subash Pathak <Subash@users.noreply.github.com>
2022-08-08 20:41:10 +0545

    Update Shells

--- src/readme.md
+++ src/readme.md
@@ -1,6 +1,7 @@
 - Copy everything from `https://github.com/desktop/desktop/tree/development/app/src/lib/shells` to `./shells`
-- Current files are at commit `1444a78057d964989c686974af8e4d65439f5a36`
+- Current files are at commit `c9a66c6b8654b0111e41ee01735a45a6bf0bd26c`
 - Find and replace `__DARWIN__` with `process.platform === 'darwin'`
 - Find and replace `__WIN32__` with `process.platform === 'win32'`
 - Find and replace `__LINUX__` with `process.platform === 'linux'`
 - Add `import log from '../log'` at the top of `shared.ts`, `win32.ts` and `is-git-on-path.ts`. ie, all the files using `log.*` methods
+- Change `import { pathExists } from '../../ui/lib/path-exists';` to `import { pathExists } from '../path-exists'`
--- src/shells/win32.ts
+++ src/shells/win32.ts
@@ -1,13 +1,12 @@
 import { spawn, ChildProcess } from 'child_process';
 import * as Path from 'path';
 import { enumerateValues, HKEY, RegistryValueType } from 'registry-js';
-import { pathExists } from 'fs-extra';
-
 import { assertNever } from '../fatal-error';
 import { IFoundShell } from './found-shell';
 import { enableWSLDetection } from '../feature-flag';
 import { findGitOnPath } from '../is-git-on-path';
 import { parseEnumValue } from '../enum';
+import { pathExists } from '../path-exists';
 import log from '../log';
 
 export enum Shell {
@@ -19,6 +18,7 @@ export enum Shell {
   Cygwin = 'Cygwin',
   WSL = 'WSL',
   WindowTerminal = 'Windows Terminal',
+  FluentTerminal = 'Fluent Terminal',
   Alacritty = 'Alacritty'
 }
 
@@ -103,6 +103,14 @@ export async function getAvailableShells(): Promise<
     shells.push({
       shell: Shell.WindowTerminal,
       path: windowsTerminal
+    });
+  }
+
+  const fluentTerminal = await findFluentTerminal();
+  if (fluentTerminal != null) {
+    shells.push({
+      shell: Shell.FluentTerminal,
+      path: fluentTerminal
     });
   }
   return shells;
@@ -364,6 +372,26 @@ async function findWindowsTerminal(): Promise<string | null> {
   return null;
 }
 
+async function findFluentTerminal(): Promise<string | null> {
+  // Fluent Terminal has a link at
+  // C:\Users\<User>\AppData\Local\Microsoft\WindowsApps\flute.exe
+  const localAppData = process.env.LocalAppData;
+  if (localAppData != null) {
+    const fluentTerminalpath = Path.join(
+      localAppData,
+      '\\Microsoft\\WindowsApps\\flute.exe'
+    );
+    if (await pathExists(fluentTerminalpath)) {
+      return fluentTerminalpath;
+    } else {
+      log.debug(
+        `[Fluent Terminal] flute.exe doest not exist at '${fluentTerminalpath}'`
+      );
+    }
+  }
+  return null;
+}
+
 export function launch(
   foundShell: IFoundShell<Shell>,
   path: string
@@ -440,6 +468,10 @@ export function launch(
       const windowsTerminalPath = `"${foundShell.path}"`;
       log.info(`launching ${shell} at path: ${windowsTerminalPath}`);
       return spawn(windowsTerminalPath, ['-d .'], { shell: true, cwd: path });
+    case Shell.FluentTerminal:
+      const fluentTerminalPath = `"${foundShell.path}"`;
+      log.info(`launching ${shell} at path: ${fluentTerminalPath}`);
+      return spawn(fluentTerminalPath, ['new'], { shell: true, cwd: path });
     default:
       return assertNever(shell, `Unknown shell: ${shell}`);
   }
